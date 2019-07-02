7512a1ba3de5324ec5285964a7285814716059b1
KenBreaker <xkosher00@gmail.com>
2019-07-02 05:15:28 -0400

    asd

--- reports/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReportConfig(AppConfig):
-    name = 'reports'
-    verbose_name = 'Gestor de reportes'
--- tree/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TreeConfig(AppConfig):
-    name = 'tree'
-    verbose_name = 'Gestor de árboles'
